9e3e61557ee1f3bc422f1d34255b832a8c472a86
LahkLeKey <LahkLeKey@gmail.com>
2022-07-25 20:33:52 -0500

    Lifecycle setup

--- ScryARK/ScryARK.razor.cs
+++ ScryARK/ScryARK.razor.cs
@@ -1,5 +1,36 @@
-﻿namespace LahkLeScryARK.ScryARK;
+﻿using System.Diagnostics;
+using System.Timers;
+using LahkLeDiscord;
+using Timer = System.Timers.Timer;
+
+namespace LahkLeScryARK.ScryARK;
 
 public partial class ScryARK
 {
+    private static DiscordBot.Instance instance = new();
+    private static Timer aTimer;
+
+    public static void Lifecycle()
+    {
+        aTimer = new Timer(1);
+
+        aTimer.Elapsed += OnElapsed;
+        aTimer.AutoReset = true;
+        aTimer.Enabled = true;
+
+        GC.KeepAlive(aTimer);
+        Debug.WriteLine("ScryARK:Main");
+    }
+
+    private static void OnElapsed(object source, ElapsedEventArgs e)
+    {
+        Debug.WriteLine("The Elapsed event was raised at {0}", e.SignalTime);
+        instance.Tick();
+    }
+
+    protected override void OnInitialized()
+    {
+        Lifecycle();
+        Debug.WriteLine("ScryARK:OnInitialized");
+    }
 }
