38acb7c1c10a1fa7ab876d7710040e573a9d904a
LahkLeKey <LahkLeKey@gmail.com>
2022-07-25 19:15:26 -0500

    Add project files.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace LahkLeScryARK;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
---
+++ Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace LahkLeScryARK;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
